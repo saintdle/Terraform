891b1e9cadadf002953d72c9f3b8ca43f0ed2398
Dean <22192242+saintdle@users.noreply.github.com>
2022-12-06 12:20:59 +0000

    Update main.tf

--- vra-vm-deploy/main.tf
+++ vra-vm-deploy/main.tf
@@ -23,7 +23,6 @@ data "vsphere_network" "network" {
 
 resource "vsphere_virtual_machine" "vm" {
   name             = "terraform-test"
-  resource_pool_id = data.vsphere_resource_pool.pool.id
   datastore_id     = data.vsphere_datastore.datastore.id
 
   num_cpus = 2
